eae2d8dccb6307c92149fc5c96f37a1699cf8aa2
MatveyDDRD <pavlovmatvej708@gmail.com>
2024-09-15 21:55:47 +0500

    im dumbass

---
+++ src/api_client.h
@@ -0,0 +1,13 @@
+#ifndef API_CLIENT_H
+
+// struct that have data of bar
+typedef struct {
+    float open;
+    float high;
+    float low;
+    float close;
+    float volume;
+} bar;
+
+#define API_CLIENT_H
+#endif
